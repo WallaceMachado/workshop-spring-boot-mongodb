03e43a2cbee11550e87cf19af6602396d4454050
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2020-11-14 17:41:40 -0300

    Obtendo um post por id

--- src/main/java/com/wallacemachado/workshopmongo/services/PostService.java
+++ src/main/java/com/wallacemachado/workshopmongo/services/PostService.java
@@ -1,5 +1,22 @@
 package com.wallacemachado.workshopmongo.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wallacemachado.workshopmongo.Repositories.PostRepository;
+import com.wallacemachado.workshopmongo.domain.Post;
+import com.wallacemachado.workshopmongo.services.exception.ObjetoNaoEncontradoException;
+
+@Service
 public class PostService {
 
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjetoNaoEncontradoException("Objeto não encontrado"));
+	}
 }
