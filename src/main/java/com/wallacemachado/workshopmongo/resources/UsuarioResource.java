59049776e62056a30d866005b42e004b658d11fd
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2020-11-14 09:55:37 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/wallacemachado/workshopmongo/resources/UsuarioResource.java
+++ src/main/java/com/wallacemachado/workshopmongo/resources/UsuarioResource.java
@@ -3,6 +3,7 @@ package com.wallacemachado.workshopmongo.resources;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wallacemachado.workshopmongo.DTO.UsuarioDTO;
 import com.wallacemachado.workshopmongo.domain.Usuario;
 import com.wallacemachado.workshopmongo.services.UsuarioService;
 
@@ -23,8 +25,11 @@ public class UsuarioResource {
 	private UsuarioService service; // injeção de de pendência
 	
 	@RequestMapping(method=RequestMethod.GET) // responde a solicitação do tipo GET (poderia ser @GETMapping 
- 	public ResponseEntity<List<Usuario>> findAll() {
-		List<Usuario> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+ 	public ResponseEntity<List<UsuarioDTO>> findAll() {
+		List<Usuario> list = service.findAll(); // lista com os usuarios do Banco
+		
+		// converte a lista com os usuarios do Banco para um alista de usuario DTO (customizados os dados que serão exibidos)
+		List<UsuarioDTO> listDto = list.stream().map(x -> new UsuarioDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
